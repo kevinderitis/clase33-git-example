268449f00de3c2cd913dae4f6cb55cd7ae41e7a9
kevinderitis <kevin.deritis@hotmail.com>
2023-01-03 20:54:33 -0300

    PORT changed

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send('Ruta raiz')
 } )
 
-app.listen(8080, ()=> {
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, ()=> {
     console.log('Server running')
 })
