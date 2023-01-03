0d458f283f550438eea7e2c8ebb1db1327e35c9f
kevinderitis <kevin.deritis@hotmail.com>
2023-01-03 20:11:42 -0300

    Creacion de otra ruta distinta

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Ruta raiz')
 } )
 
+app.get('/otra', (req, res) => {
+    res.send('otra')
+})
+
 app.listen(8080, ()=> {
     console.log('Server running')
 })
