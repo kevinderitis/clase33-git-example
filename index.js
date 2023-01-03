2f0d225ba2052d47a62f09a7134b9d9e4cfdd8e8
kevinderitis <kevin.deritis@hotmail.com>
2023-01-03 20:53:05 -0300

    Revert "Creacion de otra ruta distinta"
    This reverts commit 0d458f283f550438eea7e2c8ebb1db1327e35c9f.

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
     res.send('Ruta raiz')
 } )
 
-app.get('/otra', (req, res) => {
-    res.send('otra')
-})
-
 app.listen(8080, ()=> {
     console.log('Server running')
 })
