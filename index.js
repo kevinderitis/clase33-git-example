b50f6921182bea88334ebfbb51b8a89cfffc80ba
kevinderitis <kevin.deritis@hotmail.com>
2023-01-03 20:20:02 -0300

    Revert "Creacion de ruta raiz"
    This reverts commit add092b692712dfda26e3b506bb33b129d386aed.

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Ruta raiz')
-} )
-
 app.listen(8080, ()=> {
     console.log('Server running')
 })
