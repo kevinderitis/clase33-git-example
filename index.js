fdd3308cf30c539a7c0bb822692f385407b1e0ac
kevinderitis <kevin.deritis@hotmail.com>
2023-01-03 20:11:06 -0300

    Creacion de ruta raiz

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res) => {
+    res.send('Ruta raiz')
+} )
+
 app.listen(8080, ()=> {
     console.log('Server running')
 })
